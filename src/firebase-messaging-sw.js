170cebc9547702e27455953095c1eff535e7224f
rodri-alfonso <rodrigo.alfonso.mm@gmail.com>
2024-03-21 15:25:35 -0300

    update sw

--- src/firebase-messaging-sw.js
+++ src/firebase-messaging-sw.js
@@ -1,35 +1,3 @@
-// import { initializeApp } from "firebase/app";
-// import { getMessaging } from "firebase/messaging/sw";
-
-// const firebaseApp = initializeApp({
-//   apiKey: "AIzaSyAn_eNxjBILtjtjt7kqWPdCMNvnLAT8KcM",
-//   authDomain: "anthelios-test-notifications.firebaseapp.com",
-//   projectId: "anthelios-test-notifications",
-//   storageBucket: "anthelios-test-notifications.appspot.com",
-//   messagingSenderId: "89838469457",
-//   appId: "1:89838469457:web:4f8c6cd1f2389ebf301370",
-//   databaseURL: "https://anthelios-test-notifications.firebaseio.com",
-// });
-
-// export const messaging = getMessaging(firebaseApp);
-
-// import { onBackgroundMessage } from "firebase/messaging/sw";
-
-// onBackgroundMessage(messaging, (payload) => {
-//   console.log(
-//     "[firebase-messaging-sw.js] Received background message ",
-//     payload
-//   );
-//   // Customize notification here
-//   const notificationTitle = "Background Message Title";
-//   const notificationOptions = {
-//     body: "Background Message body.",
-//     icon: "/firebase-logo.png",
-//   };
-
-//   self.registration.showNotification(notificationTitle, notificationOptions);
-// });
-
 importScripts(
   "https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js"
 );
@@ -38,22 +6,71 @@ importScripts(
 );
 
 firebase.initializeApp({
-  apiKey: "AIzaSyAn_eNxjBILtjtjt7kqWPdCMNvnLAT8KcM",
-  authDomain: "anthelios-test-notifications.firebaseapp.com",
-  projectId: "anthelios-test-notifications",
-  storageBucket: "anthelios-test-notifications.appspot.com",
-  messagingSenderId: "89838469457",
-  appId: "1:89838469457:web:4f8c6cd1f2389ebf301370",
+  apiKey: "AIzaSyApTkEUlCPLmxSbX0i3shEKzg_5GoZ3EQE",
+  projectId: "anthelios-league-cde14",
+  messagingSenderId: "118900342700",
+  appId: "1:118900342700:web:11a0a76c4c336a41544409",
+  storageBucket: "anthelios-league-cde14.appspot.com",
+  authDomain: "anthelios-league-cde14.firebaseapp.com",
   databaseURL: "https://anthelios-test-notifications.firebaseio.com",
 });
+
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
-  const { notification } = payload;
-  // Customize notification here
-  const notificationTitle = notification.title;
-  const notificationOptions = {
-    body: notification.body,
-  };
-  self.registration.showNotification(notificationTitle, notificationOptions);
+messaging.onBackgroundMessage();
+
+const CACHE_NAME = "anthelios-league-v1.2.3";
+
+const cacheUrls = ["/", "/src/assets/"];
+
+self.addEventListener("install", (ev) => {
+  console.log(ev);
+
+  caches
+    .open(CACHE_NAME)
+    .then((cache) => {
+      return cache.addAll(cacheUrls);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
+self.addEventListener("activate", (ev) => {
+  console.log(ev);
+
+  // Limpiar caches antiguos
+  const limpiarCachePr = caches.keys().then((names) => {
+    const limpiarViejosPr = names.map((name) => {
+      if (name !== CACHE_NAME) {
+        return caches.delete(name);
+      }
+    });
+  });
+
+  ev.waitUntil(limpiarCachePr);
+});
+
+self.addEventListener("fetch", (ev) => {
+  console.log("fetching...");
+  // Esto lo que hace es revisar dentro del cache si existe la url (de cacheUrl) solicitada,
+  //  caso contrario la pedirá al servidor
+  const responsePromise = caches
+    .match(ev.request)
+    .then((response) => {
+      if (response) {
+        return response;
+      }
+
+      return fetch(ev.request);
+    })
+    .catch((error) => {
+      // Aqui manejamos cuando no hay internet, qué mostrar
+      console.error(error);
+      if (ev.request.mode === "navigate") {
+        return caches.match("./src/assets/offline.html");
+      }
+    });
+
+  ev.respondWith(responsePromise);
 });
